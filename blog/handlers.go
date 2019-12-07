007ae763ab05ada2afb80ce1c5c455011dbb9693
Randall Svancara <randalls@linux01>
2019-12-07 00:14:50 +0000

    clean up login, add additional templates

--- blog/handlers.go
+++ blog/handlers.go
@@ -5,9 +5,8 @@ import (
 	"net/http"
 	"time"
 
+	"bf.go/blog/session"
 	"github.com/flosch/pongo2"
-	"github.com/gomodule/redigo/redis"
-	uuid "github.com/satori/go.uuid"
 )
 
 // ContextKey key used by contexts to uniquely identify attributes
@@ -18,79 +17,36 @@ var users = map[string]string{
 	"user2": "password2",
 }
 
-// Credentials Create a struct that models the structure of a user, both in the request body, and in the DB
-type Credentials struct {
-	Password string `json:"password"`
-	Username string `json:"username"`
-}
-
-func getRedisConn() redis.Conn {
-	// Establish a connection to the Redis server listening on port
-	// 6379 of the local machine. 6379 is the default port, so unless
-	// you've already changed the Redis configuration file this should
-	// work.
-	conn, err := redis.Dial("tcp", "10.152.64.116:32777")
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Importantly, use defer to ensure the connection is always
-	// properly closed before exiting the main() function.
-
-	return conn
-}
-
 // AuthHandler authorize user
 func AuthHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// We can obtain the session token from the requests cookies, which come with every request
-		c, err := r.Cookie("session_token")
+		var sess session.Session
+
+		err := sess.Session(r)
 
 		if err != nil {
-			if err == http.ErrNoCookie {
-				// If the cookie is not set, return an unauthorized status
-				//w.WriteHeader(http.StatusUnauthorized)
-				//fmt.Fprintf(w, "Error %s", err)
-				http.Redirect(w, r, "/signin", http.StatusSeeOther)
-				return
-			}
-			// For any other type of error, return a bad request status
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Error  %s", err)
-			return
-		}
-		sessionToken := c.Value
-
-		cache := GetRedisConn()
-
-		defer cache.Close()
-
-		// We then get the name of the user from our cache, where we set the session token
-		response, err := cache.Do("GET", sessionToken)
-		if err != nil {
-			// If there is an error fetching from cache, return an internal server error status
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if response == nil {
-			// If the session token is not present in cache, return an unauthorized error
-			w.WriteHeader(http.StatusUnauthorized)
-			return
+			http.Redirect(w, r, "/signin", http.StatusSeeOther)
 		}
 
 		h.ServeHTTP(w, r)
-
 	})
 }
 
 // HomeHandler Home page
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
+	var sess session.Session
+	err := sess.Session(r)
+	if err != nil {
+		fmt.Printf("error getting session %s \n", err)
+		//http.Redirect(w, r, "/signin", http.StatusSeeOther)
+		//return
+	}
 
 	template := "templates/index.html"
 	tmpl := pongo2.Must(pongo2.FromFile(template))
 
-	out, err := tmpl.Execute(pongo2.Context{"title": "Index", "greating": "Hello"})
+	out, err := tmpl.Execute(pongo2.Context{"title": "Index", "greating": "Hello", "user": sess.User.Username})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Println(err)
@@ -102,14 +58,13 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 // Signin Sign into the application
 func Signin(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println(r.Method)
 	if r.Method == http.MethodPost {
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
 
-		var creds Credentials
+		var creds session.Credentials
 		creds.Username = r.FormValue("username")
 		creds.Password = r.FormValue("password")
 
@@ -124,41 +79,27 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		cache := GetRedisConn()
-		defer cache.Close()
+		var sess session.Session
 
-		// Create a new random session token
-		sessionToken := uuid.NewV4().String()
-		// Set the token in the cache, along with the user whom it represents
-		// The token has an expiry time of 120 seconds
-		_, err := cache.Do("SETEX", sessionToken, "120", creds.Username)
+		err := sess.Create(creds)
 		if err != nil {
-			// If there is an error in setting the cache, return an internal server error
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			fmt.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
 		}
 
 		// Finally, we set the client cookie for "session_token" as the session token we just generated
 		// we also set an expiry time of 120 seconds, the same as the cache
 		http.SetCookie(w, &http.Cookie{
 			Name:    "session_token",
-			Value:   sessionToken,
+			Value:   sess.SessionToken,
 			Expires: time.Now().Add(120 * time.Second),
 		})
 
-		template := "templates/index.html"
-		tmpl := pongo2.Must(pongo2.FromFile(template))
-
-		out, err := tmpl.Execute(pongo2.Context{"title": "Index", "greating": "Hello"})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			fmt.Println(err)
-		}
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, out)
+		http.Redirect(w, r, "/admin", http.StatusSeeOther)
+		return
 
 	} else {
+
 		template := "templates/signin.html"
 		tmpl := pongo2.Must(pongo2.FromFile(template))
 
@@ -170,120 +111,44 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, out)
 	}
-
 }
 
-func Welcome(w http.ResponseWriter, r *http.Request) {
+// AdminHome admin home page
+func AdminHome(w http.ResponseWriter, r *http.Request) {
 
-	// We can obtain the session token from the requests cookies, which come with every request
-	c, err := r.Cookie("session_token")
+	var sess session.Session
+	err := sess.Session(r)
 	if err != nil {
-		if err == http.ErrNoCookie {
-			// If the cookie is not set, return an unauthorized status
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte(fmt.Sprintf("There was a problem: %s!", err)))
-			fmt.Println("Bad request no cookie for you!")
-
-			return
-		}
-		fmt.Println("Bad request no cookie for you!")
-		// For any other type of error, return a bad request status
-		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		http.Redirect(w, r, "/signin", http.StatusSeeOther)
 		return
 	}
-	sessionToken := c.Value
+	template := "templates/admin.html"
+	tmpl := pongo2.Must(pongo2.FromFile(template))
 
-	//var redisCtxID ContextKey = "redis"
-	//ctx := r.Context()
-
-	cache := GetRedisConn()
-
-	//var cache = (redis.Conn)(nil)
-
-	//cache = ctx.Value(redisCtxID).(redis.Conn)
-
-	if cache == nil {
-		// If there is an error fetching from cache, return an internal server error status
-		fmt.Println("Need a cache")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// We then get the name of the user from our cache, where we set the session token
-	response, err := cache.Do("GET", sessionToken)
+	out, err := tmpl.Execute(pongo2.Context{"title": "Index", "greating": "Hello", "user": sess.User.Username})
 	if err != nil {
-		// If there is an error fetching from cache, return an internal server error status
-		fmt.Println("Error? No Response from Cache")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println(err)
 	}
-	if response == nil {
-		// If the session token is not present in cache, return an unauthorized error
-		fmt.Println("Error? No Response")
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	fmt.Println("Write Someting?")
-	// Finally, return the welcome message to the user
-	w.Write([]byte(fmt.Sprintf("Welcome %s!", response)))
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, out)
+	// w.Write([]byte(fmt.Sprintf("Welcome %s!", sess.User.Username)))
 }
 
-func Refresh(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie("session_token")
-	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	sessionToken := c.Value
-
-	var redisCtxID ContextKey = "redis"
-	ctx := r.Context()
-	cache := ctx.Value(redisCtxID).(redis.Conn)
-
-	response, err := cache.Do("GET", sessionToken)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if response == nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-	// The code uptil this point is the same as the first part of the `Welcome` route
-
-	// Now, create a new session token for the current user
-	newSessionToken := uuid.NewV4().String()
-	_, err = cache.Do("SETEX", newSessionToken, "120", fmt.Sprintf("%s", response))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Delete the older session token
-	_, err = cache.Do("DEL", sessionToken)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Set the new token as the users `session_token` cookie
-	http.SetCookie(w, &http.Cookie{
-		Name:    "session_token",
-		Value:   newSessionToken,
-		Expires: time.Now().Add(120 * time.Second),
-	})
-}
-
+// AboutHandler about page
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
+
+	var sess session.Session
+	err := sess.Session(r)
+	if err != nil {
+		fmt.Printf("Session not available %s", err)
+	}
+
 	template := "templates/about.html"
 	tmpl := pongo2.Must(pongo2.FromFile(template))
 
-	out, err := tmpl.Execute(pongo2.Context{"title": "Index", "greating": "Hello"})
+	out, err := tmpl.Execute(pongo2.Context{"title": "Index", "greating": "Hello", "user": sess.User.Username})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Println(err)
---
+++ blog/session/session.go
@@ -0,0 +1,214 @@
+package session
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"bf.go/blog/cache"
+	"github.com/gomodule/redigo/redis"
+	uuid "github.com/satori/go.uuid"
+)
+
+// Credentials Create a struct that models the structure of a user, both in the request body, and in the DB
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+// Item User items
+type Item struct {
+	Key string `json:"key"`
+	Val string `json:"val"`
+}
+
+// User stores user information for a session
+type User struct {
+	Username string `json:"username"`
+	Items    []Item `json:"items"`
+}
+
+func (u *User) setItem(key string, value string) {
+	var found bool
+
+	for k := range u.Items {
+		if u.Items[k].Key == key {
+			u.Items[k].Val = value
+			found = true
+			break
+		}
+	}
+	if found == false {
+		u.Items = append(u.Items, Item{key, value})
+	}
+}
+
+func (u *User) getItem(key string) string {
+	for k := range u.Items {
+		if u.Items[k].Key == key {
+			return u.Items[k].Val
+		}
+	}
+	return ""
+}
+
+// Session a user session object
+type Session struct {
+	SessionToken string
+	IsAuth       bool
+	User         User
+}
+
+// Create a session object
+func (s *Session) Create(creds Credentials) error {
+	cache, err := cache.GetRedisConn()
+
+	defer cache.Close()
+
+	// Create a new random session token
+	s.SessionToken = uuid.NewV4().String()
+
+	var user User
+
+	user.Username = creds.Username
+
+	byteResult, err := json.Marshal(user)
+	if err != nil {
+		fmt.Printf("Error marshaling json object %s", err)
+	}
+
+	// Set the token in the cache, along with the user whom it represents
+	// The token has an expiry time of 120 seconds
+	_, err = cache.Do("SETEX", s.SessionToken, "120", string(byteResult))
+	if err != nil {
+		return fmt.Errorf("Error saving session to redis: %s", err)
+	}
+
+	return nil
+}
+
+// SetValue get the session value for provided key
+func (s *Session) SetValue(key string, value string) error {
+	if s.SessionToken == "" {
+		return fmt.Errorf("Please use Session.Session() method to instantiate the session object")
+	}
+
+	cache, err := cache.GetRedisConn()
+
+	defer cache.Close()
+
+	if err != nil {
+		return fmt.Errorf("Error connecting to redis: %s", err)
+	}
+
+	if cache == nil {
+
+		return fmt.Errorf("Error connecting to redis, empty connection object returned")
+	}
+
+	// We then get the name of the user from our cache, where we set the session token
+	response, err := redis.String(cache.Do("GET", s.SessionToken))
+
+	if err != nil {
+		return fmt.Errorf("Error retrieving user object from redis: %s", err)
+	}
+
+	user := &User{}
+
+	err = json.Unmarshal([]byte(response), user)
+
+	user.setItem(key, value)
+
+	byteUser, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("Error marshing user object to byte array json: %s", err)
+	}
+
+	response, err = redis.String(cache.Do("SET", s.SessionToken, string(byteUser)))
+	if err != nil {
+		return fmt.Errorf("Error storing object in redis: %s", err)
+	}
+
+	return nil
+}
+
+// GetValue get the session value for provided key
+func (s *Session) GetValue(key string) (string, error) {
+	if s.SessionToken == "" {
+		return "", fmt.Errorf("Please use Session.Session() method to instantiate the session object")
+	}
+
+	cache, err := cache.GetRedisConn()
+
+	defer cache.Close()
+
+	if err != nil {
+		return "", fmt.Errorf("Error connecting to redis: %s", err)
+	}
+
+	if cache == nil {
+
+		return "", fmt.Errorf("Error connecting to redis, empty connection object returned")
+	}
+
+	// We then get the name of the user from our cache, where we set the session token
+	response, err := redis.String(cache.Do("GET", s.SessionToken))
+	if err != nil {
+		return "", fmt.Errorf("Error retrieving user object from redis: %s", err)
+	}
+
+	user := &User{}
+
+	err = json.Unmarshal([]byte(response), user)
+	if err != nil {
+		return "", fmt.Errorf("Error reading json object from redis: %s", err)
+	}
+
+	val := user.getItem(key)
+
+	return val, nil
+}
+
+// Session Session Test if user is authenticated
+func (s *Session) Session(r *http.Request) error {
+	// We can obtain the session token from the requests cookies, which come with every request
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return err
+		}
+		return err
+	}
+
+	s.SessionToken = c.Value
+
+	cache, err := cache.GetRedisConn()
+
+	defer cache.Close()
+
+	if err != nil {
+		return fmt.Errorf("Error getting cache object, empty object returned: %s", err)
+	}
+
+	if cache == nil {
+		return fmt.Errorf("Error getting cache object, empty object returned")
+	}
+
+	// We then get the name of the user from our cache, where we set the session token
+	response, err := redis.String(cache.Do("GET", s.SessionToken))
+	if err != nil {
+		// If there is an error fetching from cache, return an internal server error status
+		return fmt.Errorf("Error? No Response from Cache: %s", err)
+	}
+
+	user := &User{}
+
+	err = json.Unmarshal([]byte(response), user)
+	if err != nil {
+		// If there is an error fetching from cache, return an internal server error status
+		return fmt.Errorf("Error decoding json object: %s", err)
+	}
+	s.User = *user
+
+	return nil
+}
