e6d82fcf06cb7422e527b42ab002f545c970168f
Randall Svancara <randalls@linux01>
2020-02-10 03:56:57 +0000

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,9 +9,7 @@ set -xe
 # LATEST TAG
 GIT_TAG=$(git describe --abbrev=0 --tags)
 
-# Update helm chart with current tag
-echo "apiVersion: ${GIT_TAG}" >> helm/goblog/Chart.yaml
 
-helm3 upgrade goblog helm/goblog
+helm3 upgrade goblog helm/goblog --set apiVersion=${GIT_TAG}
 
 
