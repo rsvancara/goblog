adfc4ffc15ef4929192e1b0c1cb74a0f98de6096
Randall Svancara <randalls@linux01>
2020-02-11 17:31:41 +0000

    add google tracking and fix image link bug

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,5 +12,6 @@ GIT_TAG=$(git describe --abbrev=0 --tags)
 sed -i "s/VERSION/${GIT_TAG}/g" helm/visualintrigue/Chart.yaml
 
 helm3 upgrade visualintrigue helm/visualintrigue 
+helm3 upgrade dyitinytrailer helm/dyitinytrailer 
 
 
