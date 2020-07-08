255d60349bf582a56874a9fe054acfd669e5ffa6
Randall Svancara <randalls@Randalls-iMac.local>
2020-07-08 18:01:02 +0000

    adding trying adventure

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,8 @@ GIT_TAG=$(git describe --abbrev=0 --tags)
 
 sed -i "s/VERSION/${GIT_TAG}/g" helm/visualintrigue/Chart.yaml
 sed -i "s/VERSION/${GIT_TAG}/g" helm/diytinytrailer/Chart.yaml
+sed -i "s/VERSION/${GIT_TAG}/g" helm/tryingadventure/Chart.yaml
 
 helm3 upgrade visualintrigue helm/visualintrigue 
 helm3 upgrade diytinytrailer helm/diytinytrailer 
+helm3 upgrade tryingadventure helm/tryingadventure 
