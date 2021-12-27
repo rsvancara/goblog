6d5e6bf4aabd4e70ee04eed15bfea3acecefa4e4
Randall Svancara <randalls@Randalls-Mini.internal.tryingadventure.com>
2021-12-27 08:40:04 -0800

    update serving requests message

--- cmd/goblog/main.go
+++ cmd/goblog/main.go
@@ -93,7 +93,7 @@ func main() {
 	r.Handle("/metrics", promhttp.Handler())
 	r.Use(middleware.InstrumentHandlerDuration)
 
-	fmt.Println("Now serving requests")
+	flog.Info().Msg("Now happily serving requests")
 
 	srv := &http.Server{
 		Handler: r,
